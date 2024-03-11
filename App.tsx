15eb8ce2832540118f5b69e9b87aa9b3c744bee1
Stanislav Obrazcoff <obrazcoff@gmail.com>
2024-03-12 00:51:54 +0100

    Dz2 - add button

--- App.tsx
+++ App.tsx
@@ -1,16 +1,28 @@
-import { StatusBar } from "expo-status-bar";
-import { Button, Platform, StyleSheet, Text, View } from "react-native";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
+
+// components
+import { Button } from "./shared/Button/Button";
+
+// constants
+import { Colors, Fonts, Gaps } from "./shared/tokens";
 
 export default function App() {
-  const btnColor = Platform.OS === "android" ? "#C67C4E" : "#fff";
   return (
     <View style={styles.container}>
-      <Text style={styles.mainTitle}>Одно из самых вкусных кофе в городе!</Text>
-      <Text style={styles.smallText}>
-        Свежие зёрна, настоящая арабика и бережная обжарка
-      </Text>
-      <View style={styles.button}>
-        <Button title="Начать" color={btnColor} />
+      <ImageBackground
+        source={require("./assets/start-bg.png")}
+        style={styles.background}
+        resizeMode="contain"
+      >
+        <Text style={styles.mainTitle}>
+          Одно из самых вкусных кофе в городе!
+        </Text>
+      </ImageBackground>
+      <View style={styles.bottomView}>
+        <Text style={styles.smallText}>
+          Свежие зёрна, настоящая арабика и бережная обжарка
+        </Text>
+        <Button text="Начать" />
       </View>
     </View>
   );
@@ -19,36 +31,39 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#000",
+    backgroundColor: Colors.black,
     alignItems: "center",
     justifyContent: "flex-end",
-    paddingHorizontal: 30,
   },
-  button: {
+  background: {
+    flex: 1,
+    backgroundColor: Colors.black,
+    alignItems: "center",
+    justifyContent: "flex-end",
     width: "100%",
-    height: 64,
-    backgroundColor: "#C67C4E",
-    borderRadius: 16,
-    justifyContent: "center",
+  },
+  bottomView: {
+    width: "100%",
     marginBottom: 64,
+    paddingHorizontal: 30,
   },
   mainTitle: {
     color: "#fff",
-    fontSize: 34,
+    fontSize: Fonts.f34,
     fontWeight: "600",
     textAlign: "center",
     lineHeight: 43,
     letterSpacing: 1,
-    paddingBottom: 82,
+    paddingBottom: Gaps.g8,
   },
 
   smallText: {
     color: "#A9A9A9",
-    fontSize: 14,
+    fontSize: Fonts.f14,
     fontWeight: "400",
     textAlign: "center",
     letterSpacing: 1,
     lineHeight: 20,
-    paddingBottom: 24,
+    paddingBottom: Gaps.g24,
   },
 });
